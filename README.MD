ffaa92f799da7198667a448f270631ad9900dec1
Maximilian Schwarzmüller <business@academind.com>
2017-09-05 07:43:37 +0200

    initial commit, starting setup

---
+++ README.MD
@@ -0,0 +1,7 @@
+# A simple example PWA
+This project was built as part of a YouTube video on [https://youtube.com/c/academind](https://youtube.com/c/academind).
+
+# Usage
+Run `npm install` (you need [Node.js](https://nodejs.org) for that) to install the development server which will serve the `public` folder.
+
+Once that command finished, start the server via `npm start` and view the app at [localhost:8081](http://localhost:8081)
---
+++ public/src/js/app.js
@@ -0,0 +1,24 @@
+var pwaCard = document.querySelector('#pwa');
+var pwaCardContent = pwaCard.querySelector('.card__content');
+var pwaCardDetails = pwaCard.querySelector('.card__details');
+var detailsShown = false;
+
+pwaCard.addEventListener('click', function (event) {
+  if (!detailsShown) {
+    detailsShown = true;
+    pwaCardContent.style.opacity = 0;
+    pwaCardDetails.style.display = 'block';
+    pwaCardContent.style.display = 'none';
+    setTimeout(function () {
+      pwaCardDetails.style.opacity = 1;
+    }, 300);
+  } else {
+    detailsShown = false;
+    pwaCardDetails.style.opacity = 0;
+    pwaCardContent.style.display = 'block';
+    pwaCardDetails.style.display = 'none';
+    setTimeout(function () {
+      pwaCardContent.style.opacity = 1;
+    }, 300);
+  }
+});
