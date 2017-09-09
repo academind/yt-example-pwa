392f177f4b69cc92ad27076d4bbedb30c88e27be
Maximilian Schwarzmüller <business@academind.com>
2017-09-09 13:45:46 +0200

    finished example + deployed to firebase

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,6 +3,13 @@ var pwaCardContent = pwaCard.querySelector('.card__content');
 var pwaCardDetails = pwaCard.querySelector('.card__details');
 var detailsShown = false;
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(function() {
+      console.log('SW registered');
+    });
+}
+
 pwaCard.addEventListener('click', function (event) {
   if (!detailsShown) {
     detailsShown = true;
---
+++ public/sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        // cache.add('/');
+        // cache.add('/index.html');
+        // cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/css/app.css',
+          '/src/images/pwa.jpg',
+          'https://fonts.googleapis.com/css?family=Raleway:400,700'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
